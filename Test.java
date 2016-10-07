f277e0a49e238b036088d3ec3a25b810177514b9
Dilip pashi <dilip.pashi143@gmail.com>
2016-10-07 10:45:29 +0530

    Java_assignment_3.1
    Java_assignment_3.1

---
+++ Test.java
@@ -0,0 +1,16 @@
+
+
+public class Test{
+	// Main method was missing
+public static void main(String[] args){
+// Calling print() Method	
+print(10);
+}
+
+//Defined Print Method
+static void print (int x) {
+System.out.println (x);
+// Exit Program
+System.exit(0);
+}
+}
